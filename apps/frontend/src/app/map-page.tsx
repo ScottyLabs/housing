e44eb49d3703aea1e8cd27d6d7e242a20a58d4a6
EcstaticPilot <Nikhil.ramanuja.8@gmail.com>
2026-07-10 05:05:23 -0700

    feat: placeholder maps page

---
+++ apps/frontend/src/app/map-page.tsx
@@ -0,0 +1,16 @@
+import { Link } from "react-router-dom";
+
+export default function MapPage() {
+    return (
+        <div className="flex h-full px-10 sm:px-20 md:px-30 lg:px-40 justify-left mt-[26px]">
+            <div className="flex flex-col px-0 overflow-hidden">
+                <h1 className="font-bold text-[32px] py-3 flex-shrink-0 align-left">Housing Map</h1>
+                                        <img
+                            src={"/campusmap.svg"}
+                            alt={"campus map"}
+                            className="rounded-2xl border"
+                        />
+            </div>
+        </div>
+    );
+}
