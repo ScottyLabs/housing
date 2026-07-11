0a5da75284e43e3bd31d7a5c27b6bbb202ce453c
EcstaticPilot <Nikhil.ramanuja.8@gmail.com>
2026-07-11 01:10:13 -0700

    feat: interactive svg

--- apps/frontend/src/app/map-page.tsx
+++ apps/frontend/src/app/map-page.tsx
@@ -2,13 +2,19 @@
 export default function MapPage() {
     return (
         <div className="flex h-full px-10 sm:px-20 md:px-30 lg:px-40 justify-left mt-[26px]">
-            <div className="flex flex-col px-0 overflow-hidden">
+            <div className="flex flex-col h-full px-0">
                 <h1 className="font-bold text-[32px] py-3 flex-shrink-0 align-left">Housing Map</h1>
-                                        <img
-                            src={"/campusmap.svg"}
-                            alt={"campus map"}
-                            className="rounded-2xl border"
-                        />
+                
+                    Hover over buildings
+                    <div className="py-3">
+                    <iframe
+                        src="/housingmap.svg"
+                        title="Housing map"
+                        width="1165" 
+                        height="1016"
+                        className="rounded-2xl border overflow-hidden"
+                    />
+                </div>
             </div>
         </div>
     );
