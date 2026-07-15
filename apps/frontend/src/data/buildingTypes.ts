b115600ea4ee4ce851dc4549f08b72b083638a12
Gostmeaper <johnchangcao@gmail.com>
2026-07-15 19:01:50 -0400

    fix: style changes

--- apps/frontend/src/data/buildingTypes.ts
+++ apps/frontend/src/data/buildingTypes.ts
@@ -1,134 +1,134 @@
 export enum RoomType {
-    TradSingle,
-    TradDouble,
-    TradTriple,
-    SemiSuiteSingle,
-    SemiSuiteDouble,
-    SemiSuiteTriple,
-    SemiSuiteQuad,
-    ApartmentTriple,
-    StudioApartmentSingle,
-    StudioApartmentDouble
+  TradSingle,
+  TradDouble,
+  TradTriple,
+  SemiSuiteSingle,
+  SemiSuiteDouble,
+  SemiSuiteTriple,
+  SemiSuiteQuad,
+  ApartmentTriple,
+  StudioApartmentSingle,
+  StudioApartmentDouble,
 }
 
 export enum BathroomType {
-    Communal,
-    SharedSuite,
-    Private
+  Communal,
+  SharedSuite,
+  Private,
 }
 
 export enum ACLevel {
-    None,
-    ByNecessity,
-    Window,
-    Central
+  None,
+  ByNecessity,
+  Window,
+  Central,
 }
 
 export enum LaundryLocation {
-    None,
-    Basement,
-    EachFloor,
-    InUnit
+  None,
+  Basement,
+  EachFloor,
+  InUnit,
 }
 
 export enum KitchenScope {
-    None,
-    Shared,
-    InUnit
+  None,
+  Shared,
+  InUnit,
 }
 
 export enum GenderHousing {
-    CoEd,
-    WomenOnly,
-    MenOnly,
-    GenderInclusive
+  CoEd,
+  WomenOnly,
+  MenOnly,
+  GenderInclusive,
 }
 
 export interface Bathrooms {
-    types: BathroomType[];
-    details?: string;
+  types: BathroomType[];
+  details?: string;
 }
 export interface AirConditioning {
-    level: ACLevel;
-    details?: string;
+  level: ACLevel;
+  details?: string;
 }
 export interface Kitchen {
-    scope: KitchenScope;
-    details?: string;
+  scope: KitchenScope;
+  details?: string;
 }
 export interface Laundry {
-    location: LaundryLocation;
-    details?: string;
+  location: LaundryLocation;
+  details?: string;
 }
 export interface CommonAreas {
-    hasLounge: boolean;
-    details?: string;
+  hasLounge: boolean;
+  details?: string;
 }
 export interface Gym {
-    available: boolean;
-    details?: string;
+  available: boolean;
+  details?: string;
 }
 
 export interface AmenityData {
-    roomTypes: RoomType[];
-    bathrooms: Bathrooms;
-    ac: AirConditioning;
-    kitchen: Kitchen;
-    laundry: Laundry;
-    commonAreas: CommonAreas;
-    gym: Gym;
-    genderHousing: GenderHousing;
+  roomTypes: RoomType[];
+  bathrooms: Bathrooms;
+  ac: AirConditioning;
+  kitchen: Kitchen;
+  laundry: Laundry;
+  commonAreas: CommonAreas;
+  gym: Gym;
+  genderHousing: GenderHousing;
 }
 
 export interface Accessibility {
-    wheelchairAccessible: boolean;
-    serviceAnimalFriendly: boolean;
-    groundFloorRooms: boolean;
-    strobeAlarm: boolean;
+  wheelchairAccessible: boolean;
+  serviceAnimalFriendly: boolean;
+  groundFloorRooms: boolean;
+  strobeAlarm: boolean;
 }
 
 export interface Atmosphere {
-    socialness?: number;
-    noiseLevel?: number;
+  socialness?: number;
+  noiseLevel?: number;
 }
 
 export interface Location {
-    latitude?: number;
-    longitude?: number;
-    closeBuildings: string[];
-    note?: string;
+  latitude?: number;
+  longitude?: number;
+  closeBuildings: string[];
+  note?: string;
 }
 
 export interface GalleryImage {
-    link: string;
-    description: string;
+  link: string;
+  description: string;
 }
 export interface FloorPlan {
-    link: string;
-    description: string;
-    category: "roomType" | "floor";
-    virtualTourLink?: string;
+  link: string;
+  description: string;
+  category: "roomType" | "floor";
+  virtualTourLink?: string;
 }
 export interface Media {
-    mainImage: string;
-    icon?: string;
-    photos: GalleryImage[];
-    floorPlans: FloorPlan[];
+  mainImage: string;
+  icon?: string;
+  photos: GalleryImage[];
+  floorPlans: FloorPlan[];
 }
 
 export interface Building {
-    id: string;
-    name: string;
-    media: Media;
-    amenities: AmenityData;
-    accessibility: Accessibility;
-    atmosphere: Atmosphere;
-    location: Location;
-    editorialTags?: string[];
+  id: string;
+  name: string;
+  media: Media;
+  amenities: AmenityData;
+  accessibility: Accessibility;
+  atmosphere: Atmosphere;
+  location: Location;
+  editorialTags?: string[];
 }
 
 export interface Tag {
-    id: string;
-    label: string;
-    icon: string;
+  id: string;
+  label: string;
+  icon: string;
 }
