e9ba6d8da50148156538005f8b3416d90b500cd2
Rohan <83433649+RohanSen08@users.noreply.github.com>
2026-07-23 00:58:43 -0400

    chore: more conflicts

--- apps/frontend/src/app/building-options/BuildingFilter.tsx
+++ apps/frontend/src/app/building-options/BuildingFilter.tsx
@@ -57,7 +57,6 @@ export default function BuildingFilter() {
                 options={buildlingOptions}
                 onChangeAction={handleDistanceFromChange}
                 value={distanceFromValue}
-                subtitle="First Year Housing"
               />
             </div>
 
