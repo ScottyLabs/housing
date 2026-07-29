2ac5c129e645a72a2d351e74056e9ece1ff9a3ac
Rohan <83433649+RohanSen08@users.noreply.github.com>
2026-07-28 23:13:52 -0400

    feat: mobile UI for map, dorm details

--- apps/frontend/src/app/map-page.tsx
+++ apps/frontend/src/app/map-page.tsx
@@ -1,18 +1,19 @@
 export default function MapPage() {
   return (
-    <div className="flex h-full px-10 sm:px-20 md:px-30 lg:px-40 justify-left mt-[26px]">
-      <div className="flex flex-col h-full px-0">
-        <h1 className="font-bold text-[32px] py-3 flex-shrink-0 align-left">Housing Map</h1>
+    <div className="flex md:h-full px-4 sm:px-20 md:px-30 lg:px-40 justify-left mt-[26px]">
+      <div className="flex flex-col md:h-full w-full">
+        <h1 className="font-bold text-[24px] md:text-[32px] py-3 flex-shrink-0 align-left">
+          Housing Map
+        </h1>
         Hover over buildings
-        <div className="py-3">
-          <iframe
-            src="/housingmap.svg"
-            title="Housing map"
-            width="1165"
-            height="1016"
-            scrolling="no"
-            className="rounded-2xl border"
-          />
+        <div className="py-3 w-full max-w-[1165px]">
+          <div className="w-full aspect-[1162.9287/1014.8657]">
+            <iframe
+              src="/housingmap.svg"
+              title="Housing map"
+              className="w-full h-full rounded-2xl border"
+            />
+          </div>
         </div>
       </div>
     </div>
